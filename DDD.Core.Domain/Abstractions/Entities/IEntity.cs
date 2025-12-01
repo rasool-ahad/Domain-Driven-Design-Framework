4fa68d5e00bc73255b3b583dde96ae0e3938c823
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-01 19:47:29 +0330

    complete IEntity interface

--- DDD.Core.Domain/Abstractions/Entities/IEntity.cs
+++ DDD.Core.Domain/Abstractions/Entities/IEntity.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Security.Principal;
 
-namespace DDD.Core.Domain.Abstractions.Entities
+namespace DDD.Core.Domain.Abstractions.Entities;
+
+public interface IEntity<TPrimaryKey> where TPrimaryKey : struct
 {
-    internal interface IEntity
-    {
-    }
+    TPrimaryKey Id { get; }
 }
