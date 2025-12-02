0f35acda51aeb42ebbf9e36f317e42f20df58363
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-03 01:05:32 +0330

    add IDomainEvent

--- Src/Core/DDD.Core.Domain/Abstractions/Events/IDomainEvent.cs
+++ Src/Core/DDD.Core.Domain/Abstractions/Events/IDomainEvent.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace DDD.Core.Domain.Abstractions.Events;
 
-namespace DDD.Core.Domain.Abstractions.Events
+internal interface IDomainEvent
 {
-    internal interface IDomainEvent
-    {
-    }
 }
