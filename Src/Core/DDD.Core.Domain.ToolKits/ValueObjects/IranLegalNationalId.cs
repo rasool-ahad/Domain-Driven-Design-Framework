24107fc945f94539af3143cf2fc41302cf26552c
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-03 23:02:07 +0330

    add useful valueObjects to domain toolkit

---
+++ Src/Core/DDD.Core.Domain.ToolKits/ValueObjects/IranLegalNationalId.cs
@@ -0,0 +1,92 @@
+﻿using DDD.Core.Domain.Exceptions;
+using DDD.Core.Domain.Models.ValueObjects;
+
+namespace DDD.Core.Domain.ToolKits.ValueObjects;
+
+public class IranLegalNationalId : BaseValueObject<IranLegalNationalId>
+{
+    #region Properties
+    public string Value { get; private set; }
+    #endregion
+
+    #region Constructors and Factories
+    public static IranLegalNationalId FromString(string value) => new(value);
+    private IranLegalNationalId(string value)
+    {
+        if (!IsLegalNationalIdValid(value))
+        {
+            throw new InvalidValueObjectStateException("ValidationErrorStringFormat", nameof(IranLegalNationalId));
+        }
+
+        Value = value;
+    }
+    private IranLegalNationalId()
+    {
+
+    }
+
+    #endregion
+
+    #region Equality Check
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+
+    #endregion
+
+    #region Operator Overloading
+    public static explicit operator string(IranLegalNationalId title) => title.Value;
+    public static implicit operator IranLegalNationalId(string value) => new(value);
+    #endregion
+    #region Methods
+    public override string ToString() => Value;
+
+    #endregion
+
+    private static bool IsLegalNationalIdValid(string nationalId)
+    {
+        if (string.IsNullOrWhiteSpace(nationalId) || !(nationalId.Length == 11))
+            return false;
+
+        if (!IsFormat1Validate(nationalId))
+            return false;
+
+        if (!IsFormat2Validate(nationalId))
+            return false;
+        return true;
+
+        static bool IsFormat1Validate(string nationalId)
+        {
+            var allDigitEqual = new[] { "00000000000", "11111111111", "22222222222", "33333333333", "44444444444", "55555555555", "66666666666", "77777777777", "88888888888", "99999999999" };
+            if (!allDigitEqual.Contains(nationalId))
+                return true;
+            return false;
+        }
+
+        static bool IsFormat2Validate(string nationalId)
+        {
+            var chArray = nationalId.ToCharArray();
+            var controlCode = Convert.ToInt32(nationalId[10].ToString());
+            var factor = Convert.ToInt32(nationalId[9].ToString()) + 2;
+            var sum = 0;
+            sum += (factor + Convert.ToInt32(chArray[0].ToString())) * 29;
+            sum += (factor + Convert.ToInt32(chArray[1].ToString())) * 27;
+            sum += (factor + Convert.ToInt32(chArray[2].ToString())) * 23;
+            sum += (factor + Convert.ToInt32(chArray[3].ToString())) * 19;
+            sum += (factor + Convert.ToInt32(chArray[4].ToString())) * 17;
+            sum += (factor + Convert.ToInt32(chArray[5].ToString())) * 29;
+            sum += (factor + Convert.ToInt32(chArray[6].ToString())) * 27;
+            sum += (factor + Convert.ToInt32(chArray[7].ToString())) * 23;
+            sum += (factor + Convert.ToInt32(chArray[8].ToString())) * 19;
+            sum += (factor + Convert.ToInt32(chArray[9].ToString())) * 17;
+            var remaining = sum % 11;
+            if (remaining == 10)
+                remaining = 0;
+            return remaining == controlCode;
+        }
+    }
+
+}
+
---
+++ Src/Core/DDD.Core.Domain.ToolKits/ValueObjects/NationalCode.cs
@@ -0,0 +1,93 @@
+﻿using DDD.Core.Domain.Exceptions;
+using DDD.Core.Domain.Models.ValueObjects;
+
+namespace DDD.Core.Domain.ToolKits.ValueObjects;
+
+public class IranNationalCode : BaseValueObject<IranNationalCode>
+{
+    #region Properties
+    public string Value { get; private set; }
+    #endregion
+
+    #region Constructors and Factories
+    public static IranNationalCode FromString(string value) => new(value);
+    public IranNationalCode(string value)
+    {
+        if (!IsNationalCode(value))
+        {
+            throw new InvalidValueObjectStateException("ValidationErrorStringFormat", nameof(IranNationalCode));
+        }
+
+        Value = value;
+    }
+    private IranNationalCode()
+    {
+
+    }
+    #endregion
+
+    #region Equality Check
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+
+    #endregion
+
+    #region Operator Overloading
+
+    public static explicit operator string(IranNationalCode title) => title.Value;
+    public static implicit operator IranNationalCode(string value) => new(value);
+    #endregion
+
+    #region Methods
+    public override string ToString() => Value;
+
+    public static bool IsNationalCode(string nationalCode)
+    {
+        if (string.IsNullOrWhiteSpace(nationalCode) || !(nationalCode.Length <= 12 && nationalCode.Length >= 8))
+            return false;
+
+        nationalCode = nationalCode.PadLeft(10, '0');
+
+        if (!IsFormat1Validate(nationalCode))
+            return false;
+
+        if (!IsFormat2Validate(nationalCode))
+            return false;
+        return true;
+
+        static bool IsFormat1Validate(string nationalCode)
+        {
+            var allDigitEqual = new[] { "0000000000", "1111111111", "2222222222", "3333333333", "4444444444", "5555555555", "6666666666", "7777777777", "8888888888", "9999999999" };
+            if (!allDigitEqual.Contains(nationalCode))
+                return true;
+            return false;
+        }
+
+        static bool IsFormat2Validate(string nationalCode)
+        {
+            var chArray = nationalCode.ToCharArray();
+            var num0 = Convert.ToInt32(chArray[0].ToString()) * 10;
+            var num2 = Convert.ToInt32(chArray[1].ToString()) * 9;
+            var num3 = Convert.ToInt32(chArray[2].ToString()) * 8;
+            var num4 = Convert.ToInt32(chArray[3].ToString()) * 7;
+            var num5 = Convert.ToInt32(chArray[4].ToString()) * 6;
+            var num6 = Convert.ToInt32(chArray[5].ToString()) * 5;
+            var num7 = Convert.ToInt32(chArray[6].ToString()) * 4;
+            var num8 = Convert.ToInt32(chArray[7].ToString()) * 3;
+            var num9 = Convert.ToInt32(chArray[8].ToString()) * 2;
+            var a = Convert.ToInt32(chArray[9].ToString());
+
+            var b = num0 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9;
+            var c = b % 11;
+
+            var result = c < 2 && a == c || c >= 2 && 11 - c == a;
+            if (result)
+                return true;
+            return false;
+        }
+    }
+        #endregion
+    }
+
