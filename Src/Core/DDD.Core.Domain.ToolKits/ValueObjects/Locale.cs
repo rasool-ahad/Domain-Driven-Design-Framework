a8bbac515c370718f2682d2472c9c8133f6a0c62
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-05 21:42:30 +0330

    add locale valueObject to domain toolkit

---
+++ Src/Core/DDD.Core.Domain.ToolKits/ValueObjects/Locale.cs
@@ -0,0 +1,37 @@
+﻿using DDD.Core.Domain.Models.ValueObjects;
+
+namespace DDD.Core.Domain.ToolKits.ValueObjects;
+
+
+public class Locale : BaseValueObject<Locale>
+{
+    #region Properties
+    public string Code { get; private set; }
+    #endregion
+
+    #region Constructors and Factories
+    private Locale(string code)
+    {
+        Code = code;
+    }
+
+    private Locale()
+    {
+    }
+
+    public static Locale FromString(string code) => new(code);
+    #endregion
+
+    #region Equality Check
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Code;
+    }
+    #endregion
+
+    #region Operator Overloading
+    public static explicit operator string(Locale locale) => locale.Code;
+
+    public static implicit operator Locale(string code) => new(code);
+    #endregion
+}
