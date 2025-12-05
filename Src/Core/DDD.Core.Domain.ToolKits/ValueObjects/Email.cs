572a246bb3f13342dfa49a261fabf380a735be13
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-05 21:42:19 +0330

    add email valueObject to domain toolkit

---
+++ Src/Core/DDD.Core.Domain.ToolKits/ValueObjects/Email.cs
@@ -0,0 +1,47 @@
+﻿using DDD.Core.Domain.Exceptions;
+using DDD.Core.Domain.Models.ValueObjects;
+using System.Text.RegularExpressions;
+
+namespace DDD.Core.Domain.ToolKits.ValueObjects;
+
+public class Email : BaseValueObject<Email>
+{
+    #region Properties
+    public string Value { get; private set; }
+    #endregion
+
+    #region Constructors and Factories
+    private Email(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+            throw new InvalidValueObjectStateException($"{value} cannot be null");
+
+        bool isEmail = Regex.IsMatch(value, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z",
+            RegexOptions.IgnoreCase);
+
+        if (!isEmail)
+            throw new InvalidValueObjectStateException($"{value} is not a valid email");
+
+        Value = value;
+    }
+
+    private Email()
+    {
+    }
+
+    public static Email FromString(string value) => new(value);
+    #endregion
+
+    #region Equality Check
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+    #endregion
+
+    #region Operator Overloading
+    public static explicit operator string(Email email) => email.Value;
+
+    public static implicit operator Email(string value) => new(value);
+    #endregion
+}
