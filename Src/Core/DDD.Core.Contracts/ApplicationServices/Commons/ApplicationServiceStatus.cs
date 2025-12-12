17685d3b6b024d17b5dec363d71eda06b55126fb
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-12 23:26:46 +0330

    add result pattern

---
+++ Src/Core/DDD.Core.Contracts/ApplicationServices/Commons/ApplicationServiceStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace DDD.Core.Contracts.ApplicationServices.Commons;
+
+public enum ApplicationServiceStatus
+{
+    Ok = 1,
+    NotFound = 2,
+    ValidationError = 3,
+    InvalidDomainState = 4,
+    Exception = 5,
+}
---
+++ Src/Core/DDD.Core.Contracts/ApplicationServices/Commons/IApplicationServiceResult.cs
@@ -0,0 +1,8 @@
+﻿namespace DDD.Core.Contracts.ApplicationServices.Commons;
+
+public interface IApplicationServiceResult
+{
+    IEnumerable<string> Messages { get; }
+
+    ApplicationServiceStatus Status { get; set; }
+}
