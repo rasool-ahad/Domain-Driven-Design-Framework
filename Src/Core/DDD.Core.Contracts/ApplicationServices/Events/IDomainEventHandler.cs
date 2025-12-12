9f3fd3677b49508fd9d77dc0c212803f14729b54
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-12 23:27:09 +0330

    add events application service interfaces

---
+++ Src/Core/DDD.Core.Contracts/ApplicationServices/Events/IDomainEventHandler.cs
@@ -0,0 +1,8 @@
+﻿using DDD.Core.Domain.Abstractions.Events;
+
+namespace DDD.Core.Contracts.ApplicationServices.Events;
+
+public interface IDomainEventHandler<TDomainEvent> where TDomainEvent : IDomainEvent
+{
+    Task Handle(TDomainEvent Event);
+}
---
+++ Src/Core/DDD.Core.Contracts/ApplicationServices/Events/IEventDispatcher.cs
@@ -0,0 +1,8 @@
+﻿using DDD.Core.Domain.Abstractions.Events;
+
+namespace DDD.Core.Contracts.ApplicationServices.Events;
+
+public interface IEventDispatcher
+{
+    Task PublishDomainEventAsync<TDomainEvent>(TDomainEvent @event) where TDomainEvent : class, IDomainEvent;
+}
