5dc0179b2bf2d743d96d6619c46813c8b1b8aa9d
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-14 17:43:07 +0330

    add dispatchers to application service

---
+++ Src/Core/DDD.Core.ApplicationServices/Commands/CommandDispatcher.cs
@@ -0,0 +1,55 @@
+﻿using DDD.Core.Contracts.ApplicationServices.Commands;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace DDD.Core.ApplicationServices.Commands;
+
+public class CommandDispatcher : ICommandDispatcher
+{
+    #region Fields
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<CommandDispatcher> _logger;
+    #endregion
+
+    #region Constructors
+    public CommandDispatcher(IServiceProvider serviceProvider, ILogger<CommandDispatcher> logger)
+    {
+        _serviceProvider = serviceProvider;
+        _logger = logger;
+    }
+    #endregion
+
+    #region Send Commands
+    public async Task<CommandResult> Send<TCommand>(TCommand command) where TCommand : class, ICommand
+    {
+        try
+        {
+            _logger.LogDebug("Routing command of type {CommandType} With value {Command}  Start at {StartDateTime}", command.GetType(), command, DateTime.Now);
+            var handler = _serviceProvider.GetRequiredService<ICommandHandler<TCommand>>();
+            return await handler.Handle(command);
+        }
+        catch (InvalidOperationException ex)
+        {
+            _logger.LogError(ex, "There is not suitable handler for {CommandType} Routing failed at {StartDateTime}.", command.GetType(), DateTime.Now);
+            throw;
+        }
+
+    }
+
+    public async Task<CommandResult<TData>> Send<TCommand, TData>(TCommand command) where TCommand : class, ICommand<TData>
+    {
+        try
+        {
+            _logger.LogDebug("Routing command of type {CommandType} With value {Command}  Start at {StartDateTime}", command.GetType(), command, DateTime.Now);
+            var handler = _serviceProvider.GetRequiredService<ICommandHandler<TCommand, TData>>();
+            return await handler.Handle(command);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "There is not suitable handler for {CommandType} Routing failed at {StartDateTime}.", command.GetType(), DateTime.Now);
+            throw;
+        }
+    }
+
+    #endregion
+}
---
+++ Src/Core/DDD.Core.ApplicationServices/Queries/QueryDispatcher.cs
@@ -0,0 +1,38 @@
+﻿using DDD.Core.Contracts.ApplicationServices.Queries;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace DDD.Core.ApplicationServices.Queries;
+
+public class QueryDispatcher : IQueryDispatcher
+{
+    #region Fields
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<QueryDispatcher> _logger;
+    #endregion
+
+    #region Constructors
+    public QueryDispatcher(IServiceProvider serviceProvider, ILogger<QueryDispatcher> logger)
+    {
+        _serviceProvider = serviceProvider;
+        _logger = logger;
+    }
+    #endregion
+
+    #region Query Dispatcher
+    public Task<QueryResult<TData>> Execute<TQuery, TData>(TQuery query) where TQuery : class, IQuery<TData>
+    {
+        try
+        {
+            _logger.LogDebug("Routing query of type {QueryType} With value {Query}  Start at {StartDateTime}", query.GetType(), query, DateTime.Now);
+            var handler = _serviceProvider.GetRequiredService<IQueryHandler<TQuery, TData>>();
+            return handler.Handle(query);
+        }
+        catch (InvalidOperationException ex)
+        {
+            _logger.LogError(ex, "There is not suitable handler for {QueryType} Routing failed at {StartDateTime}.", query.GetType(), DateTime.Now);
+            throw;
+        }
+    }
+    #endregion
+}
