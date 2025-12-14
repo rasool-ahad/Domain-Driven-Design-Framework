bf9a1775f5285f60d784400bef8930f1e3d28320
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-14 12:46:10 +0330

    Add command data contracts

---
+++ Src/Core/DDD.Core.Contracts/Data/Commands/ICommandRepository.cs
@@ -0,0 +1,34 @@
+﻿using DDD.Core.Domain.Models.Entities;
+using System.Linq.Expressions;
+
+namespace DDD.Core.Contracts.Data.Commands;
+
+public interface ICommandRepository<TEntity, TId> : IUnitOfWork
+    where TEntity : AggregateRoot<TId>
+     where TId : struct,
+          IComparable,
+          IComparable<TId>,
+          IConvertible,
+          IEquatable<TId>,
+          IFormattable
+{
+    void Delete(TId id);
+
+    void DeleteGraph(TId id);
+
+    void Delete(TEntity entity);
+
+    void Insert(TEntity entity);
+
+
+    Task InsertAsync(TEntity entity);
+
+    TEntity Get(TId id);
+    Task<TEntity> GetAsync(TId id);
+
+    TEntity GetGraph(TId id);
+    Task<TEntity> GetGraphAsync(TId id);
+
+    bool Exists(Expression<Func<TEntity, bool>> expression);
+    Task<bool> ExistsAsync(Expression<Func<TEntity, bool>> expression);
+}
---
+++ Src/Core/DDD.Core.Contracts/Data/Commands/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿namespace DDD.Core.Contracts.Data.Commands;
+
+public interface IUnitOfWork
+{
+    void BeginTransaction();
+
+    void CommitTransaction();
+
+    void RollbackTransaction();
+
+    int Commit();
+
+    Task<int> CommitAsync();
+}
