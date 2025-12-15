bacde95f0b5c64fb7529949d6d92d526687cbf41
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-15 22:44:04 +0330

    update applicationService layer commands

---
+++ Src/Core/DDD.Core.ApplicationServices/Commands/CommandDispatcherDecorator.cs
@@ -0,0 +1,27 @@
+﻿using DDD.Core.Contracts.ApplicationServices.Commands;
+
+namespace DDD.Core.ApplicationServices.Commands;
+
+public abstract class CommandDispatcherDecorator : ICommandDispatcher
+{
+    #region Fields
+    protected ICommandDispatcher _commandDispatcher;
+    public abstract int Order { get; }
+    #endregion
+
+    #region Constructors
+    public CommandDispatcherDecorator()
+    {
+    }
+
+    #endregion
+    public void SetCommandDispatcher(ICommandDispatcher commandDispatcher)
+    {
+        _commandDispatcher = commandDispatcher;
+    }
+    #region Abstract Send Commands
+    public abstract Task<CommandResult> Send<TCommand>(TCommand command) where TCommand : class, ICommand;
+
+    public abstract Task<CommandResult<TData>> Send<TCommand, TData>(TCommand command) where TCommand : class, ICommand<TData>;
+    #endregion
+}
---
+++ Src/Core/DDD.Core.ApplicationServices/Commands/CommandDispatcherDomainExceptionHandlerDecorator.cs
@@ -0,0 +1,101 @@
+﻿using DDD.Core.Contracts.ApplicationServices.Commands;
+using DDD.Core.Contracts.ApplicationServices.Commons;
+using DDD.Core.Domain.Exceptions;
+using Microsoft.Extensions.Logging;
+
+namespace DDD.Core.ApplicationServices.Commands;
+
+public class CommandDispatcherDomainExceptionHandlerDecorator : CommandDispatcherDecorator
+{
+    #region Fields
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<CommandDispatcherDomainExceptionHandlerDecorator> _logger;
+    #endregion
+
+    #region Constructors
+    public CommandDispatcherDomainExceptionHandlerDecorator(IServiceProvider serviceProvider,
+                                                            ILogger<CommandDispatcherDomainExceptionHandlerDecorator> logger)
+    {
+        _serviceProvider = serviceProvider;
+        _logger = logger;
+    }
+
+    public override int Order => 2;
+    #endregion
+
+    #region Send Commands
+    public override async Task<CommandResult> Send<TCommand>(TCommand command)
+    {
+        try
+        {
+            var result = _commandDispatcher.Send(command);
+            return await result;
+        }
+        catch (DomainStateException ex)
+        {
+            _logger.LogError(ex, "Processing of {CommandType} With value {Command} failed at {StartDateTime} because there are domain exceptions.", command.GetType(), command, DateTime.Now);
+            return DomainExceptionHandlingWithoutReturnValue<TCommand>(ex);
+        }
+        catch (AggregateException ex)
+        {
+            if (ex.InnerException is DomainStateException domainStateException)
+            {
+                _logger.LogError(domainStateException, "Processing of {CommandType} With value {Command} failed at {StartDateTime} because there are domain exceptions.", command.GetType(), command, DateTime.Now);
+                return DomainExceptionHandlingWithoutReturnValue<TCommand>(domainStateException);
+            }
+            throw;
+        }
+
+    }
+
+    public override async Task<CommandResult<TData>> Send<TCommand, TData>(TCommand command)
+    {
+        try
+        {
+            var result = await _commandDispatcher.Send<TCommand, TData>(command);
+            return result;
+
+        }
+        catch (DomainStateException ex)
+        {
+            _logger.LogError(ex, "Processing of {CommandType} With value {Command} failed at {StartDateTime} because there are domain exceptions.", command.GetType(), command, DateTime.Now);
+            return DomainExceptionHandlingWithReturnValue<TCommand, TData>(ex);
+        }
+        catch (AggregateException ex)
+        {
+            if (ex.InnerException is DomainStateException domainStateException)
+            {
+                _logger.LogError(ex, "Processing of {CommandType} With value {Command} failed at {StartDateTime} because there are domain exceptions.", command.GetType(), command, DateTime.Now);
+                return DomainExceptionHandlingWithReturnValue<TCommand, TData>(domainStateException);
+            }
+            throw;
+        }
+    }
+    #endregion
+
+    #region Privaite Methods
+    private CommandResult DomainExceptionHandlingWithoutReturnValue<TCommand>(DomainStateException ex)
+    {
+        var commandResult = new CommandResult
+        {
+            Status = ApplicationServiceStatus.InvalidDomainState
+        };
+
+        commandResult.AddMessage("domain exception");
+
+        return commandResult;
+    }
+
+    private CommandResult<TData> DomainExceptionHandlingWithReturnValue<TCommand, TData>(DomainStateException ex)
+    {
+        var commandResult = new CommandResult<TData>()
+        {
+            Status = ApplicationServiceStatus.InvalidDomainState
+        };
+
+        commandResult.AddMessage("domain exception");
+
+        return commandResult;
+    }
+    #endregion
+}
