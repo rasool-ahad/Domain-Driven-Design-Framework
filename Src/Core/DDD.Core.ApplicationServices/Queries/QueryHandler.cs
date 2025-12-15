5699ecde3af646588e7fafa6cec6debaaca3a6e8
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-15 22:45:03 +0330

    update applicationServiceLayer queries

---
+++ Src/Core/DDD.Core.ApplicationServices/Queries/QueryHandler.cs
@@ -0,0 +1,43 @@
+﻿using DDD.Core.Contracts.ApplicationServices.Commons;
+using DDD.Core.Contracts.ApplicationServices.Queries;
+
+namespace DDD.Core.ApplicationServices.Queries;
+
+public abstract class QueryHandler<TQuery, TData> : IQueryHandler<TQuery, TData>
+    where TQuery : class, IQuery<TData>
+{
+    protected readonly QueryResult<TData> result = new();
+
+    protected virtual Task<QueryResult<TData>> ResultAsync(TData data, ApplicationServiceStatus status)
+    {
+        result._data = data;
+        result.Status = status;
+        return Task.FromResult(result);
+    }
+
+    protected virtual QueryResult<TData> Result(TData data, ApplicationServiceStatus status)
+    {
+        result._data = data;
+        result.Status = status;
+        return result;
+    }
+
+    protected virtual Task<QueryResult<TData>> ResultAsync(TData data)
+    {
+        var status = data != null ? ApplicationServiceStatus.Ok : ApplicationServiceStatus.NotFound;
+        return ResultAsync(data, status);
+    }
+
+    protected virtual QueryResult<TData> Result(TData data)
+    {
+        var status = data != null ? ApplicationServiceStatus.Ok : ApplicationServiceStatus.NotFound;
+        return Result(data, status);
+    }
+
+    protected void AddMessage(string message)
+    {
+        result.AddMessage(message);
+    }
+
+    public abstract Task<QueryResult<TData>> Handle(TQuery query);
+}
