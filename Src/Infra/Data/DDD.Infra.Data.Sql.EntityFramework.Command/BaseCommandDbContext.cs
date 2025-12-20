ffbb5197bcedb08229ae8465189f511e1f53f32a
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-20 22:48:52 +0330

    add baseCommandDbContext

---
+++ Src/Infra/Data/DDD.Infra.Data.Sql.EntityFramework.Command/BaseCommandDbContext.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Globalization;
+
+namespace DDD.Infra.Data.Sql.EntityFramework.Commands;
+
+public abstract class BaseCommandDbContext : DbContext
+{
+    protected IDbContextTransaction? _transaction;
+
+    public BaseCommandDbContext(DbContextOptions options) : base(options)
+    {
+
+    }
+
+    protected BaseCommandDbContext()
+    {
+    }
+
+    public void BeginTransaction()
+    {
+        _transaction = Database.BeginTransaction();
+    }
+
+    public void RollbackTransaction()
+    {
+        if (_transaction == null)
+        {
+            throw new NullReferenceException("Please call `BeginTransaction()` method first.");
+        }
+        _transaction.Rollback();
+    }
+
+    public void CommitTransaction()
+    {
+        if (_transaction == null)
+        {
+            throw new NullReferenceException("Please call `BeginTransaction()` method first.");
+        }
+        _transaction.Commit();
+    }
+
+    public T? GetShadowPropertyValue<T>(object entity, string propertyName) where T : IConvertible
+    {
+        var value = Entry(entity).Property(propertyName).CurrentValue;
+        return value != null
+            ? (T)Convert.ChangeType(value, typeof(T), CultureInfo.InvariantCulture)
+            : default;
+    }
+
+    public object GetShadowPropertyValue(object entity, string propertyName)
+    {
+        return Entry(entity).Property(propertyName).CurrentValue!;
+    }
+
+    public IEnumerable<string> GetIncludePaths(Type clrEntityType)
+    {
+        var entityType = Model.FindEntityType(clrEntityType)!;
+        var includedNavigations = new HashSet<INavigation>();
+        var stack = new Stack<IEnumerator<INavigation>>();
+        while (true)
+        {
+            var entityNavigations = new List<INavigation>();
+            foreach (var navigation in entityType.GetNavigations())
+            {
+                if (includedNavigations.Add(navigation))
+                    entityNavigations.Add(navigation);
+            }
+            if (entityNavigations.Count == 0)
+            {
+                if (stack.Count > 0)
+                    yield return string.Join(".", stack.Reverse().Select(e => e.Current.Name));
+            }
+            else
+            {
+                foreach (var navigation in entityNavigations)
+                {
+                    var inverseNavigation = navigation.Inverse;
+                    if (inverseNavigation != null)
+                        includedNavigations.Add(inverseNavigation);
+                }
+                stack.Push(entityNavigations.GetEnumerator());
+            }
+            while (stack.Count > 0 && !stack.Peek().MoveNext())
+                stack.Pop();
+            if (stack.Count == 0) break;
+            entityType = stack.Peek().Current.TargetEntityType;
+        }
+    }
+}
