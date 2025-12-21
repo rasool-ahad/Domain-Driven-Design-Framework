0dad6e1e083095b881b2559b503f58371c3e2df6
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-21 13:41:08 +0330

    add dependencyInjections to webApi

---
+++ Src/EndPoints/DDD.Endpoints.WebApi/Extensions/DependencyInjections/AddApplicationServicesExtensions.cs
@@ -0,0 +1,62 @@
+﻿using DDD.Core.ApplicationServices.Commands;
+using DDD.Core.Contracts.ApplicationServices.Commands;
+using DDD.Core.Contracts.ApplicationServices.Events;
+using DDD.Core.Contracts.ApplicationServices.Queries;
+using FluentValidation;
+
+namespace DDD.Endpoints.WebApi.Extensions.DependencyInjections;
+
+public static class AddApplicationServicesExtensions
+{
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services,
+                                                                 IEnumerable<Assembly> assembliesForSearch)
+        => services.AddCommandHandlers(assembliesForSearch)
+                   .AddCommandDispatcherDecorators()
+                   .AddQueryHandlers(assembliesForSearch)
+                   .AddEventHandlers(assembliesForSearch)
+                   .AddFluentValidators(assembliesForSearch);
+
+    private static IServiceCollection AddCommandHandlers(this IServiceCollection services, IEnumerable<Assembly> assembliesForSearch)
+        => services.AddWithTransientLifetime(assembliesForSearch, typeof(ICommandHandler<>), typeof(ICommandHandler<,>));
+
+    private static IServiceCollection AddCommandDispatcherDecorators(this IServiceCollection services)
+    {
+        services.AddTransient<CommandDispatcher, CommandDispatcher>();
+        services.AddTransient<CommandDispatcherDecorator, CommandDispatcherDomainExceptionHandlerDecorator>();
+        services.AddTransient<CommandDispatcherDecorator, CommandDispatcherValidationDecorator>();
+
+        services.AddTransient<ICommandDispatcher>(c =>
+        {
+            var commandDispatcher = c.GetRequiredService<CommandDispatcher>();
+            var decorators = c.GetServices<CommandDispatcherDecorator>().ToList();
+            if (decorators?.Any() == true)
+            {
+                decorators = decorators.OrderBy(c => c.Order).ToList();
+                var listFinalIndex = decorators.Count - 1;
+                for (int i = 0; i <= listFinalIndex; i++)
+                {
+                    if (i == listFinalIndex)
+                    {
+                        decorators[i].SetCommandDispatcher(commandDispatcher);
+
+                    }
+                    else
+                    {
+                        decorators[i].SetCommandDispatcher(decorators[i + 1]);
+                    }
+                }
+                return decorators[0];
+            }
+            return commandDispatcher;
+        });
+        return services;
+    }
+
+    private static IServiceCollection AddQueryHandlers(this IServiceCollection services, IEnumerable<Assembly> assembliesForSearch)
+        => services.AddWithTransientLifetime(assembliesForSearch, typeof(IQueryHandler<,>));
+
+    private static IServiceCollection AddEventHandlers(this IServiceCollection services, IEnumerable<Assembly> assembliesForSearch)
+        => services.AddWithTransientLifetime(assembliesForSearch, typeof(IDomainEventHandler<>));
+    private static IServiceCollection AddFluentValidators(this IServiceCollection services, IEnumerable<Assembly> assembliesForSearch)
+        => services.AddValidatorsFromAssemblies(assembliesForSearch);
+}
---
+++ Src/EndPoints/DDD.Endpoints.WebApi/Extensions/DependencyInjections/AddDataAccessExtentsions.cs
@@ -0,0 +1,20 @@
+﻿using DDD.Core.Contracts.Data.Commands;
+using DDD.Core.Contracts.Data.Queries;
+
+namespace DDD.Endpoints.WebApi.Extensions.DependencyInjections;
+
+public static class AddDataAccessExtentsions
+{
+    public static IServiceCollection AddDataAccess(
+        this IServiceCollection services,
+        IEnumerable<Assembly> assembliesForSearch) =>
+        services.AddRepositories(assembliesForSearch).AddUnitOfWorks(assembliesForSearch);
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services,
+        IEnumerable<Assembly> assembliesForSearch) =>
+        services.AddWithTransientLifetime(assembliesForSearch, typeof(ICommandRepository<,>), typeof(IQueryRepository));
+
+    public static IServiceCollection AddUnitOfWorks(this IServiceCollection services,
+        IEnumerable<Assembly> assembliesForSearch) =>
+        services.AddWithTransientLifetime(assembliesForSearch, typeof(IUnitOfWork));
+}
