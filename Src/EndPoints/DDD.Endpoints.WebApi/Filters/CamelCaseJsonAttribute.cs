9151b9a658ec53bbb5440be09b43901dcb40c82f
Rasool Ahadi <rasoolahad1997.work@gmail.com>
2025-12-29 23:48:12 +0330

    Add Filters

---
+++ Src/EndPoints/DDD.Endpoints.WebApi/Filters/CamelCaseJsonAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using System.Text.Json;
+using System.Text.Json.Serialization.Metadata;
+
+namespace DDD.Endpoints.WebApi.Filters;
+
+[AttributeUsage(AttributeTargets.Method)]
+public class CamelCaseJsonAttribute : Attribute
+{
+}
+
+public class CamelCaseJsonFilter : IAsyncResultFilter
+{
+    public async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+    {
+        var hasCamelCaseAttr = context.ActionDescriptor.EndpointMetadata
+            .OfType<CamelCaseJsonAttribute>()
+            .Any();
+
+        if (hasCamelCaseAttr && context.Result is ObjectResult objectResult)
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                TypeInfoResolver = new DefaultJsonTypeInfoResolver()
+            };
+
+            objectResult.Formatters.Clear();
+            objectResult.Formatters.Add(new SystemTextJsonOutputFormatter(options));
+        }
+
+        await next();
+    }
+}
+
